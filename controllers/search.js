e125d845986a858d4298fa06eebe22a5994bc174
bcbell <coleman.britt@gmail.com>
2020-12-30 11:00:18 -0500

    Updated: description properly rendering

--- controllers/search.js
+++ controllers/search.js
@@ -12,6 +12,6 @@ function index(req, res){
 function jobQuery(req, res){
     axios.get(`https://jobs.github.com/positions.json?page=1&${req.body.query}`)
     .then((response)=>{
-        res.render('cover', {title: "The Job Buzz ", position: response.data})
+        res.render('results', {title: "The Job Buzz ", position: response.data})
 })
 }
